fdb8563d41497f28783bebf3162a29c3eb4a9b48
eleonoraronchini <eleonora.ronchini@outlook.it>
2024-10-16 19:42:16 +0200

    ads

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-Button = document.getElementById("addtaskButton")
-Button.addEventListener("click", function () {
-    const taskInput = document.getElementById ("taskInput");
-    const taskValue = taskInput.value;
-    if (taskValue) {
-        const newTask = document.createElement("li");
-        newTask.textContent = taskValue;
-    }
-})
+const TaskBtn = document.getElementById("new-task-button");
+const input = document.getElementById("new-task");
+const ul = document.getElementById("taskList");
 
+console.log (TaskBtn);
+
+TaskBtn.onclick = function () {
+   if (input.value.length > 0) {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    ul.appendChild(li);
+    input.value = ""; /*per svuotare la barra dell'Input dopo aver premuto "agggiungi Task" */
+   
+   const deleteButton = document.createElement("button");
+   deleteButton.textContent = "Done";
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click",function(){
+        li.classList.add("style");
+     }); 
+};
+return;
+};
+const container = document.getElementsByClassName("container");
+container.style.textAlign = "center";
